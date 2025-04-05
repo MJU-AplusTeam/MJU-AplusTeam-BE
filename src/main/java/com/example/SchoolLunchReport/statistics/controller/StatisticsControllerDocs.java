d577902d1121acead568f11cb651ffe4cc8170b3
0702Yoon <gkstkddbs99@naver.com>
2025-04-05 20:18:57 +0900

    [Feat] 주간,월간 통계 조회 API 구현

--- src/main/java/com/example/SchoolLunchReport/statistics/controller/StatisticsControllerDocs.java
+++ src/main/java/com/example/SchoolLunchReport/statistics/controller/StatisticsControllerDocs.java
@@ -1,15 +1,20 @@
 package com.example.SchoolLunchReport.statistics.controller;
 
 import com.example.SchoolLunchReport.global.response.ApiResponse;
-import com.example.SchoolLunchReport.statistics.domain.type.Period;
+import com.example.SchoolLunchReport.statistics.domain.type.PeriodType;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDate;
 
 @Tag(name = "순위 관련된 api")
 public interface StatisticsControllerDocs {
 
     @Operation(summary = "인기, 비인기 5개 메뉴 조회 api")
-    ApiResponse<?> getRankMenu(Period period);
+    ApiResponse<?> getRankMenu(PeriodType periodType, LocalDate conditionDate);
 
+    @Operation(summary = "주간, 월간 통계 조회 api")
     ApiResponse<?> getStatistics();
+
+    @Operation(summary = "급 상승 메뉴 조회")
+    ApiResponse<?> getTrendingMenu(PeriodType periodType);
 }
--- src/main/java/com/example/SchoolLunchReport/statistics/service/StatisticsService.java
+++ src/main/java/com/example/SchoolLunchReport/statistics/service/StatisticsService.java
@@ -1,15 +1,18 @@
 package com.example.SchoolLunchReport.statistics.service;
 
-import static com.example.SchoolLunchReport.statistics.domain.type.Period.MONTHLY;
-import static com.example.SchoolLunchReport.statistics.domain.type.Period.WEEKLY;
+import static com.example.SchoolLunchReport.statistics.domain.type.PeriodType.MONTHLY;
+import static com.example.SchoolLunchReport.statistics.domain.type.PeriodType.WEEKLY;
 
 import com.example.SchoolLunchReport.statistics.controller.dto.response.CombinedStatisticsResponse;
+import com.example.SchoolLunchReport.statistics.controller.dto.response.RankMenuResponseDto;
 import com.example.SchoolLunchReport.statistics.controller.dto.response.StatisticsResponse;
 import com.example.SchoolLunchReport.statistics.domain.entity.FeedBack;
+import com.example.SchoolLunchReport.statistics.domain.type.RankType;
 import com.example.SchoolLunchReport.statistics.impl.FeedBackImpl;
-import com.example.SchoolLunchReport.statistics.controller.dto.response.RankMenuResponseDto;
-import com.example.SchoolLunchReport.statistics.domain.type.Period;
+import com.example.SchoolLunchReport.statistics.controller.dto.response.CombinedRankMenuResponseDto;
+import com.example.SchoolLunchReport.statistics.domain.type.PeriodType;
 import com.example.SchoolLunchReport.statistics.impl.RankImpl;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,10 +24,14 @@ public class StatisticsService {
     final FeedBackImpl feedBackImpl;
     final RankImpl rankImpl;
 
-    public RankMenuResponseDto getRankMenu(Period period) {
-        List<FeedBack> feedBackList = feedBackImpl.getFeedBackBoundary(period);
-
-        return rankImpl.generateRankResponses(feedBackList, 5);
+    public CombinedRankMenuResponseDto getRankMenu(PeriodType periodType, LocalDate conditionDate) {
+        List<RankMenuResponseDto> topRankMenuResponseDtos = rankImpl.generateRankResponse(
+            periodType,
+            conditionDate, RankType.TOP);
+        List<RankMenuResponseDto> bottomRankMenuResponseDtos = rankImpl.generateRankResponse(
+            periodType,
+            conditionDate, RankType.BOTTOM);
+        return CombinedRankMenuResponseDto.of(topRankMenuResponseDtos, bottomRankMenuResponseDtos);
     }
 
 
@@ -38,5 +45,10 @@ public class StatisticsService {
             feedBackListMonthly,
             MONTHLY);
         return new CombinedStatisticsResponse(statisticsResponseWeekly, statisticsResponseMonthly);
+    }
+
+    public RankMenuResponseDto getTrendingMenu(PeriodType periodType) {
+        List<FeedBack> feedBackList = feedBackImpl.getFeedBackBoundary(periodType);
+        return rankImpl.getTrendingMenu(feedBackList, periodType);
     }
 }
