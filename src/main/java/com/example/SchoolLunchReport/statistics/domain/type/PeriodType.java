ce93a467e86c64809465989e969c5e442816c34a
0702Yoon <gkstkddbs99@naver.com>
2025-04-05 20:21:03 +0900

    PeriodType 네임 변경

--- src/main/java/com/example/SchoolLunchReport/statistics/domain/type/PeriodType.java
+++ src/main/java/com/example/SchoolLunchReport/statistics/domain/type/PeriodType.java
@@ -2,13 +2,13 @@ package com.example.SchoolLunchReport.statistics.domain.type;
 
 import java.time.LocalDate;
 
-public enum Period {
+public enum PeriodType {
     WEEKLY, MONTHLY;
     private final static int PERIOD_BOUNDARY = 1;
 
-    public static LocalDate getConditionDate(Period period) {
+    public static LocalDate getConditionDate(PeriodType periodType) {
         LocalDate today = LocalDate.now();
-        return switch (period) {
+        return switch (periodType) {
             case WEEKLY -> today.minusWeeks(PERIOD_BOUNDARY);
             case MONTHLY -> today.minusMonths(PERIOD_BOUNDARY);
         };
